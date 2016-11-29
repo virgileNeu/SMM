47589fe942329cb4e6dde8f60fbbcfc492b8e59c
Simon Narduzzi <simon.narduzzi@epfl.ch>
2016-11-30 00:25:30 +0100

    Code reformating

--- Scripts/ArtistExtractor.py
+++ Scripts/ArtistExtractor.py
@@ -8,195 +8,200 @@ import matplotlib.pyplot as plt
 
 def updateDictionnary(genres,dictionnary,debug = False):
 	'''This method update the dictionary using the genres list'''
-    for genre in genres:
-        genre = genre.lower()
-        if(not genre in dictionnary):
-            #gs = itertools.combinations(genre.split(" "),i)
-            spacesplit = genre.split(" ")
-            gs = []
-            for a in spacesplit:
-                linesplit = a.split("-")
-                for l in linesplit:
-                    gs.append(str(l))
-                
-            mainGenre = None
-            for g in gs:
-                #g = str(g).replace("', '"," ").replace("')","").replace("('","").replace("',)","")
-                if(g in dictionnary):
-                    if(not genre in dictionnary):
-                        mainGenre = dictionnary[g]
-                        dictionnary.update({genre : mainGenre})   
-                        if(debug):            
-                            print("added : dic{"+genre+" : "+mainGenre+"}")
-                        #return dictionnary
-                else:
-                    if(len(g)>1):
-                        dictionnary.update({g : g})
-                        if(debug):
-                            print("added : dic{"+g+" : "+g+"}")
-                #if(genre in dictionnary):
-                     #   return dictionnary
-                    #else:
-                       # dictionnary.update({genre : genre)
-        if(genre not in dictionnary):  
-            if(debug):
-                print("missing genre : "+genre)
-            dictionnary.update({genre : genre})
-
-    return dictionnary
+	if(genres==None):
+		return dictionnary
+		
+	for genre in genres:
+		genre = genre.lower()
+		if(not genre in dictionnary):
+			#gs = itertools.combinations(genre.split(" "),i)
+			spacesplit = genre.split(" ")
+			gs = []
+			for a in spacesplit:
+				linesplit = a.split("-")
+				for l in linesplit:
+					gs.append(str(l))
+				
+			mainGenre = None
+			for g in gs:
+				#g = str(g).replace("', '"," ").replace("')","").replace("('","").replace("',)","")
+				if(g in dictionnary):
+					if(not genre in dictionnary):
+						mainGenre = dictionnary[g]
+						dictionnary.update({genre : mainGenre})   
+						if(debug):            
+							print("added : dic{"+genre+" : "+mainGenre+"}")
+						#return dictionnary
+				else:
+					if(len(g)>1):
+						dictionnary.update({g : g})
+						if(debug):
+							print("added : dic{"+g+" : "+g+"}")
+				#if(genre in dictionnary):
+					#   return dictionnary
+					#else:
+						# dictionnary.update({genre : genre)
+		if(genre not in dictionnary):  
+			if(debug):
+				print("missing genre : "+genre)
+			dictionnary.update({genre : genre})
+			
+	return dictionnary
 
 def createDictionnary(debug = False):
 	'''Create the dictionnary of genres'''
-    #wikiURL = "https://en.wikipedia.org/wiki/"+str(genre)
+	#wikiURL = "https://en.wikipedia.org/wiki/"+str(genre)
 	listGenres = "https://en.wikipedia.org/wiki/List_of_electronic_music_genres"
 	contentWIKI = requests.get(listGenres).text
-    #content = requests.get(listGenres).text
-    soup = bs4.BeautifulSoup(contentWIKI,"html5lib")
-    mainList = []
-    subgenres = []
-    dic = {}
-    main = ""
-    
-    for div in soup.findAll(attrs={"style" : "-moz-column-width: 20em; -webkit-column-width: 20em; column-width: 20em;"}):   
-        if(div.attrs['style'] == "-moz-column-width: 20em; -webkit-column-width: 20em; column-width: 20em;"):
-             if('style' in div.attrs):
-                uls = div.find('ul')
-                if(uls is not None):
-                    for groups in uls:
-                        for a in groups:
-                            b = str(a)
-                            soup2 = bs4.BeautifulSoup(b,"html5lib")
-                            if(len(soup2.text)>0 and soup2.text.count("\n")<2):
-                                mainList.append(soup2.text)
-                                main = soup2.text.replace("\n","")
-                                dic.update({main.lower() : main.lower()})
-                                if(debug):print("MAIN "+str(len(main))+" "+main)
+	#content = requests.get(listGenres).text
+	soup = bs4.BeautifulSoup(contentWIKI,"html5lib")
+	mainList = []
+	subgenres = []
+	dic = {}
+	main = ""
+	
+	for div in soup.findAll(attrs={"style" : "-moz-column-width: 20em; -webkit-column-width: 20em; column-width: 20em;"}):   
+		if(div.attrs['style'] == "-moz-column-width: 20em; -webkit-column-width: 20em; column-width: 20em;"):
+			if('style' in div.attrs):
+				uls = div.find('ul')
+				if(uls is not None):
+					for groups in uls:
+						for a in groups:
+							b = str(a)
+							soup2 = bs4.BeautifulSoup(b,"html5lib")
+							if(len(soup2.text)>0 and soup2.text.count("\n")<2):
+								mainList.append(soup2.text)
+								main = soup2.text.replace("\n","")
+								dic.update({main.lower() : main.lower()})
+								if(debug):print("MAIN "+str(len(main))+" "+main)
                                 
-                            if("ul" in b and len(b)>0):
-                                soup2 = bs4.BeautifulSoup(b,"html5lib")
-                                if(len(soup2.text)>0):
-                                    t = soup2.text
-                                    lines = t.split("\n")
-                                    for genre in lines:
-                                        if(len(genre)>0):
-                                            if(debug):
-                                                print(genre +"-"+main)
-                                            if(debug):
-                                                print("...")
-                                            dic.update({genre.lower() : main.lower()})
+							if("ul" in b and len(b)>0):
+								soup2 = bs4.BeautifulSoup(b,"html5lib")
+								if(len(soup2.text)>0):
+									t = soup2.text
+									lines = t.split("\n")
+									for genre in lines:
+										if(len(genre)>0):
+											if(debug):
+												print(genre +"-"+main)
+											if(debug):
+												print("...")
+											dic.update({genre.lower() : main.lower()})
                                 
        
-    ##Manual Add
-    dic.update({"house" : "house music"})
-    dic.update({"tech" : "techno"})
-    dic.update({"funk rock" : "funk"})
-    dic.update({"trap francais" : "rap"})
-    dic.update({"catstep":"drum and bass"})
-    dic.update({"dance pop" : "pop"})
-    dic.update({"complextro" : "uk garage"})
-    dic.update({"hip hop" : "hip hop"})
-    dic.update({"hip" : "hip hop"})
-    dic.update({"hop" : "hip hop"})
-    dic.update({"rock" : "rock"})
-    dic.update({"rap" : "rap"})
-    dic.update({"filthstep" : "uk garage"})
-    
-    return dic
+	##Manual Add
+	dic.update({"house" : "house music"})
+	dic.update({"tech" : "techno"})
+	dic.update({"funk rock" : "funk"})
+	dic.update({"trap francais" : "rap"})
+	dic.update({"catstep":"drum and bass"})
+	dic.update({"dance pop" : "pop"})
+	dic.update({"complextro" : "uk garage"})
+	dic.update({"hip hop" : "hip hop"})
+	dic.update({"hip" : "hip hop"})
+	dic.update({"hop" : "hip hop"})
+	dic.update({"rock" : "rock"})
+	dic.update({"rap" : "rap"})
+	dic.update({"filthstep" : "uk garage"})
+	
+	return dic
 	
 	
 def sanitize(genres):
 	'''This method remove unwanted features from the list of genres'''
-    if(genres is None):
-        return genre
-    else:
-        gen = []
-        
-        nations = ["italian","autralian","french","english","greek","latin"]
-        for genre in genres:
-            gs = genre.split(" ")
-            valid = True
-            for g in gs:
-                if(g in nations):
-                     valid = False
-            if(valid):
-                gen.append(genre)
-    return gen
+	if(genres is None):
+		return genres
+	else:
+		gen = []
+		
+		nations = ["italian","autralian","french","english","greek","latin"]
+		for genre in genres:
+			gs = genre.split(" ")
+			valid = True
+			for g in gs:
+				if(g in nations):
+					valid = False
+			if(valid):
+				gen.append(genre)
+	return gen
 
 def getMaxGenre(genres,debug = False):
 	'''Get the genre in genres that appears the most'''
-    dicnum = {}
-    for g in genres :
-        dicnum.update({g : genres.count(g)})
+	dicnum = {}
+	for g in genres :
+		dicnum.update({g : genres.count(g)})
    
-    maxKey = 0
-    maxEntry = None
-    
-    if(debug):
-        print(dicnum)
-        plt.bar(range(len(dicnum)), dicnum.values(), align='center')
-        plt.xticks(range(len(dicnum)), dicnum.keys())
+	maxKey = 0
+	maxEntry = None
+	
+	if(debug):
+		print(dicnum)
+		plt.bar(range(len(dicnum)), dicnum.values(), align='center')
+		plt.xticks(range(len(dicnum)), dicnum.keys())
         
-    for entry in dicnum :
-        if dicnum[entry]>maxKey and entry is not None:
-            maxEntry = entry
-            maxKey = dicnum[entry]
-    
-    return maxEntry     
+	for entry in dicnum :
+		if dicnum[entry]>maxKey and entry is not None:
+			maxEntry = entry
+			maxKey = dicnum[entry]
+	
+	return maxEntry     
 	
 def mainGenres(genres,dictionnary=None, debug = False):
-    '''This function associates a genre with the best match in dictionnary'''
-        dictionnary = updateDictionnary(genres,dictionnary, debug)
-        
-        mainGenreAssociates = []
-        for genre in genres :
-            #Update the list with the new genre
-            #if(genre not in dictionnary):
-               # dictionnary = updateDictionnary(genre,dictionnary, debug)
-                    
+	'''This function associates a genre with the best match in dictionnary'''
+	dictionnary = updateDictionnary(genres,dictionnary, debug)
+		
+	mainGenreAssociates = []
+	if(genres==None):
+		return mainGenreAssociates
+		
+	for genre in genres :
+		#Update the list with the new genre
+		#if(genre not in dictionnary):
+			# dictionnary = updateDictionnary(genre,dictionnary, debug)        
     
-             #mainGenreAssociates.append(associates[genre])
-            if(debug):
-                print("for "+str(genre)+" found "+dictionnary[genre])
-            mainGenreAssociates.append(dictionnary[genre])
-            
-
-        return mainGenreAssociates
+		#mainGenreAssociates.append(associates[genre])
+		if(debug):
+			print("for "+str(genre)+" found "+dictionnary[genre])
+		mainGenreAssociates.append(dictionnary[genre])
+				
+	return mainGenreAssociates
 		
 def getGenresFromSpotify(Artist):
 	'''Uses the SpotifyAPI to get a list of genres of an Artist'''
-    SpotifySearchURL = "https://api.spotify.com/v1/search"
-    
-    #arguments
-    args = {
+	SpotifySearchURL = "https://api.spotify.com/v1/search"
+	
+	#arguments
+	args = {
 		'q' : str(Artist),
 		'type' : 'artist'
-    }
-    
-    #Query 
-    content = json.loads(requests.get(SpotifySearchURL, params = args).text)
-    
-    genres = content['artists']['items'][0]['genres']
-    return genres
+	}
+	
+	#Query 
+	content = json.loads(requests.get(SpotifySearchURL, params = args).text)
+	genres = None
+	try:
+		genres = content['artists']['items'][0]['genres']
+	except:
+		genres = None
+	return genres
 	
 def getGenre(Artist,dictionnary = None, Year=None, debug= False):
-    '''Get the genre of an artist at this period.If period is None, 
-    gets the most representative genre of the artist
-    across time.'''
-    if(dictionnary is None):
-        dictionnary = createDictionnary()
-    
-    genres = getGenresFromSpotify(Artist)
-    genres = sanitize(genres)
-    
-    MG = mainGenres(genres,dictionnary, debug)
-    
-    #print("Genres : "+str(genres))
-    #print("mainGenres : "+str(MG))
-    
-    
-    mainGenre = getMaxGenre(MG,debug)
-    
-    return mainGenre
+	'''Get the genre of an artist at this period.If period is None, 
+	gets the most representative genre of the artist across time.'''
+	if(dictionnary==None):
+		dictionnary = createDictionnary()
+	
+	genres = getGenresFromSpotify(Artist)
+	genres = sanitize(genres)
+	
+	MG = mainGenres(genres,dictionnary, debug)
+	
+	#print("Genres : "+str(genres))
+	#print("mainGenres : "+str(MG))
+		
+	mainGenre = getMaxGenre(MG,debug)
+	
+	return mainGenre
 	
 def getMeanGenre(genres):
 	'''If an artist has a lot of genres, try to find the more representative'''
@@ -205,4 +210,4 @@ def getMeanGenre(genres):
 def getDiscography(Artist,Year = None):
 	'''return the discography of the Artist'''
 	#TODO
-    return
+	return
