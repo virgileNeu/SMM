c9d1571cbb6850114931565fe843bbbf472d286b
Simon Narduzzi <simon.narduzzi@epfl.ch>
2017-01-29 16:30:05 +0100

    Created run.py and adapted extraction scripts to external call

--- Scripts/AggregateData.py
+++ Scripts/AggregateData.py
@@ -3,18 +3,23 @@ import os
 import pandas as pd
 import numpy as np
 
+def aggregate(path,outputfile,encoding="utf-8"):
+	print("Aggregating events data from folder : "+path)
+	df = None
+	file_list = sorted(os.listdir(path))
+	for csv_file in file_list:
+		if (df is None):
+			df = pd.read_csv(path + '/' + csv_file,encoding=encoding)
+		else:
+			tmp = pd.read_csv(path + '/' + csv_file,encoding=encoding)
+			df = df.append(tmp, ignore_index=True)
+
+	df.to_csv(outputfile,encoding=encoding)
+	print("Aggregation saved to : "+outputfile)
 
 if __name__ == "__main__":
     if(len(sys.argv) < 3):
         sys.exit("ERROR : not enough arguments. Require folder and output file")
     path = sys.argv[1]
     outputfile = sys.argv[2]
-    df = None
-    file_list = sorted(os.listdir(path))
-    for csv_file in file_list:
-        if (df is None):
-            df = pd.read_csv(path + '/' + csv_file)
-        else:
-            tmp = pd.read_csv(path + '/' + csv_file)
-            df = df.append(tmp, ignore_index=True)
-    df.to_csv(outputfile)
+    aggregate(path,outputfile,encoding = "utf-8")
---
+++ Scripts/run.py
@@ -0,0 +1,27 @@
+import sys
+from datetime import datetime, date, timedelta
+import numpy as np
+import pandas as pd
+import EventsChDataExtractor
+import ResidentAdvisor_scrapping
+import AggregateData
+
+if __name__ == "__main__":
+	print("-----EVENTCH EXTRACTOR-----")
+	startDateObject = date(2016,11,22)
+	endDateObject = date(2017,1,29)
+	
+	eventsch_path_folder = "EventsChDataTest/"
+	merging_path_folder = "MergingTest/"
+	
+	outputfile_eventsch = merging_path_folder+"EventsMerged.csv"
+	output_file =merging_path_folder+"AllMerged.csv"
+	
+	maxPage = 1
+	EventsChDataExtractor.getEventsBetween(startDateObject,endDateObject,maxPage,eventsch_path_folder)
+	AggregateData.aggregate(eventsch_path_folder,outputfile_eventsch)
+	
+	filename = merging_path_folder+"ClubMerged.csv"
+	ResidentAdvisor_scrapping.scrap(filename, start=0,end=2,encoding = "utf-8")
+	
+	AggregateData.aggregate(merging_path_folder,output_file)
