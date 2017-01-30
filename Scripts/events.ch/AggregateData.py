d495cccd1adf108803eb527f4fe1f986cb6caf93
virgile neu <virgile.neu@gmail.com>
2017-01-30 16:05:16 +0100

    add script to update every month

--- Scripts/events.ch/AggregateData.py
+++ Scripts/events.ch/AggregateData.py
@@ -4,18 +4,20 @@ import pandas as pd
 import numpy as np
 
 def aggregate(path,outputfile,encoding="utf-8"):
-	print("Aggregating events data from folder : "+path)
-	df = None
-	file_list = sorted(os.listdir(path))
-	for csv_file in file_list:
-		if (df is None):
-			df = pd.read_csv(path + '/' + csv_file,encoding=encoding)
-		else:
-			tmp = pd.read_csv(path + '/' + csv_file,encoding=encoding)
-			df = df.append(tmp, ignore_index=True)
+    print("Aggregating events data from folder : "+path)
+    df = None
+    file_list = sorted(os.listdir(path))
+    for csv_file in file_list:
+        if(csv_file[0].isdigit()):
+            #only take YYYY-MM-DD.csv file (check first char is digit)
+            if(df is None):
+                df = pd.read_csv(path + '/' + csv_file,encoding=encoding)
+            else:
+                tmp = pd.read_csv(path + '/' + csv_file,encoding=encoding)
+                df = df.append(tmp, ignore_index=True)
 
-	df.to_csv(outputfile,encoding=encoding)
-	print("Aggregation saved to : "+outputfile)
+    df.to_csv(outputfile,encoding=encoding)
+    print("Aggregation saved to : "+outputfile)
 
 # small function to merge all the small csv
 if __name__ == "__main__":
